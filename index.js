7af338a59f8d5986aa50ab602bac5c9715aa0cec
afrizaloky <afrizaloky@outlook.com>
2021-10-23 13:49:31 +0700

    change port

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const downgrade = require("downgrade");
 const app = express();
-const config = require("./config");
 const api = require("./api");
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+
 app.use(express.json({ extended: false }));
 app.use("/api/", api);
 app.get("/", function (req, res) {
   res.json({ status: 200 });
 });
 app.listen(config.port, function () {
-  console.log("App running on port " + config.port + "!");
+  console.log("App running on port " + port + "!");
   downgrade();
 });
